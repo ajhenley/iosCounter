7834578992bd168b432c1855f34e49ebb41cb7d5
Aj Henley <ajhenley@gmail.com>
2018-06-19 02:45:17 -0400

    added just number field

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentNumber = 0
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var justNumberLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        myLabel.text = String(currentNumber)
+        justNumberLabel.text = String(currentNumber)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func nextNumber(_ sender: Any) {
+        var mess:String = ""
+        currentNumber += 1
+        if currentNumber % 3 == 0 && currentNumber % 5 == 0 {
+            mess = "FizzBuzz"
+        } else if currentNumber % 3 == 0 {
+            mess = "Fizz"
+        } else if currentNumber % 5 == 0 {
+            mess = "Buzz"
+        } else {
+            mess = String(currentNumber)
+        }
+        myLabel.text = mess
+        justNumberLabel.text = String(currentNumber)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
